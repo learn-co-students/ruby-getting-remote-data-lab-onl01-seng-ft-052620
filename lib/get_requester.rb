7c70e06f3bf5d00589aca45bc949861bf0d82b44
seanbalke <sean.balke@gmail.com>
2020-06-12 12:21:17 -0500

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,33 @@
-# Write your code here
+require 'net/http'
+require 'json'
+require 'open-uri'
+require 'pry'
+
+class GetRequester
+
+    attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end
+
+
+    def get_response_body
+        #sends the GET request to the URL passed on init. should return the BODY of the response
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+
+    end
+
+
+    def parse_json
+        #use get_response_body request, return a ruby array or hash made up of data converted from response of request
+        programs = JSON.parse(self.get_response_body)
+        binding.pry
+        programs
+
+    end
+
+
+end
